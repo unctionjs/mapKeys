5520c078605c29cc610435e48867dba07401fff7
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import mapKeysWithValueKey from "@unction/mapkeyswithvaluekey"
-
-export default function mapKeys (unction: MapperFunctionType): UnaryFunctionType {
-  return function mapKeysUnction (functor: KeyedFunctorType): KeyedFunctorType {
-    return mapKeysWithValueKey((): UnaryFunctionType => (key: KeyType): ValueType => unction(key))(functor)
-  }
+import mapKeysWithValueKey from "@unction/mapkeyswithvaluekey";
+export default function mapKeys(unction) {
+  return function mapKeysUnction(functor) {
+    return mapKeysWithValueKey(() => key => unction(key))(functor);
+  };
 }
